b8282ad89bf5a930010680b614040ffb5c05df27
WilO22 <wilmer.ogosi.22@gmail.com>
2025-10-21 17:10:34 -0500

    feat(core): configurar rutas y layouts anidados (public, admin, login)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
+// src/app/app.routes.ts
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+   
+    loadComponent: () => import('./features/auth/login-page/login-page')
+  },
+  {
+    path: 'admin',
+    
+    loadComponent: () => import('./features/admin/admin-layout/admin-layout'),
+    children: [
+      {
+        path: 'dashboard',
+        
+        loadComponent: () => import('./features/admin/pages/dashboard-page/dashboard-page')
+      },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: '', 
+   
+    loadComponent: () => import('./features/public/public-layout/public-layout'),
+    children: [
+      {
+        path: '', 
+        
+        loadComponent: () => import('./features/public/pages/catalog-page/catalog-page')
+      }
+    ]
+  }
+];
---
+++ src/app/features/public/pages/catalog-page/catalog-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-page',
+  imports: [],
+  templateUrl: './catalog-page.html',
+  styleUrl: './catalog-page.css'
+})
+export default class CatalogPage {
+
+}
+
+
