01cdd4d4999270bf5922afd1a6bf3a91f6143b2d
WilO22 <wilmer.ogosi.22@gmail.com>
2025-10-28 17:44:04 -0500

    feat(firebase): integrar @angular/fire con Auth, Firestore y Storage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "peru-tradicion-c26e2", appId: "1:692450878113:web:361fbabd983b2a352146e0", storageBucket: "peru-tradicion-c26e2.firebasestorage.app", apiKey: "AIzaSyCupzIWND9s9QcBTR3MgLS3ZqCAPR6kFxk", authDomain: "peru-tradicion-c26e2.firebaseapp.com", messagingSenderId: "692450878113", projectNumber: "692450878113", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
   ]
 };
