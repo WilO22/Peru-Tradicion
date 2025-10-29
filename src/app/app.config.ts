480a348a0bb92a6f25076e2582f96010dd5ee710
WilO22 <wilmer.ogosi.22@gmail.com>
2025-10-28 19:00:38 -0500

    fix(core): usar el provider estable 'provideZonelessChangeDetection' en app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,59 @@
+<<<<<<< HEAD
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+=======
+// import {
+//   ApplicationConfig,
+//   provideBrowserGlobalErrorListeners,
+//   provideZonelessChangeDetection,
+// } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { getAuth, provideAuth } from '@angular/fire/auth';
+// import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+// import { getStorage, provideStorage } from '@angular/fire/storage';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZonelessChangeDetection(),
+//     provideRouter(routes),
+//     provideClientHydration(withEventReplay()),
+//     provideFirebaseApp(() =>
+//       initializeApp({})
+//     ),
+//     provideAuth(() => getAuth()),
+//     provideFirestore(() => getFirestore()),
+//     provideStorage(() => getStorage()),
+//   ],
+// };
+
+
+// src/app/app.config.ts
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection, // <-- El nombre correcto (estable) que tú descubriste
+} from '@angular/core';
+>>>>>>> bd22973 (fix(core): usar el provider estable 'provideZonelessChangeDetection' en app.config.ts)
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+<<<<<<< HEAD
+=======
+
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+// Nuestro refactor de environment
+import { environment } from '../environments/environment.development';
+
+// Imports de Firebase
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -12,6 +63,16 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "peru-tradicion-c26e2", appId: "1:692450878113:web:361fbabd983b2a352146e0", storageBucket: "peru-tradicion-c26e2.firebasestorage.app", apiKey: "AIzaSyCupzIWND9s9QcBTR3MgLS3ZqCAPR6kFxk", authDomain: "peru-tradicion-c26e2.firebaseapp.com", messagingSenderId: "692450878113", projectNumber: "692450878113", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    // Proveedores de Firebase con nuestro environment
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebase)
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
+    provideStorage(() => getStorage()),
